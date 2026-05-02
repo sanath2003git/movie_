f1ea96a2707d37026ec4a933c860dfcb65f1b38c
sanath2003git <165697113+sanath2003git@users.noreply.github.com>
2026-05-02 16:26:42 +0530

    Create first reusable React component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,24 @@ import './App.css'
 
 function App() {
   return (
+    <>
       <div>
-        <h1>Movie App</h1>
+      <h1>Movie App</h1>
       <p>Welcome to React Journey</p>
       </div>
+      <Text/>
+    </>
+      
   )
+}
+function Text(){
+  return(
+    <div>
+      <p>
+        hello world
+      </p>
+    </div>
+  );
 }
 
 export default App
