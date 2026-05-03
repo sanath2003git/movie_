1b3f19ec0ace0342d9be65fea22c7b2fb89f4d82
sanath2003git <165697113+sanath2003git@users.noreply.github.com>
2026-05-03 19:46:23 +0530

    Added anime movie data

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import "../css/Navbar.css"
 function NavBar() {
     return <nav className="navbar">
         <div className="navbar-brand">
-            <Link to="/">Movie App</Link>
+            <Link to="/">Anime Movie</Link>
         </div>
         <div className="navbar-links">
             <Link to="/" className="nav-link">Home</Link>
