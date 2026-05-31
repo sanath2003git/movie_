203ef37bd71386fb7d7554aaa2a3e7af83cc9c2e
sanath2003git <165697113+sanath2003git@users.noreply.github.com>
2026-05-31 15:14:47 +0530

    Add backend search and genre filtering

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -11,10 +11,16 @@ def movie_list(request):
     movies = Movie.objects.all()
 
     search = request.GET.get("search")
+    genre = request.GET.get("genre")
 
     if search:
         movies = movies.filter(
             title__icontains=search
+        )
+
+    if genre and genre != "All":
+        movies = movies.filter(
+            genre=genre
         )
 
     serializer = MovieSerializer(
